3a8768670c6bea11a6d03c487cd130a7ab69d7b9
Pranoy Majumdar <works.pranoy@gmail.com>
2025-08-23 17:06:01 +0530

    feat(ui): add dropdown menu, label, and toaster components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./components/badge";
 export * from "./components/button";
 export * from "./components/card";
+export * from "./components/dropdown-menu";
 export * from "./components/input";
+export * from "./components/label";
+export * from "./components/sonner";
